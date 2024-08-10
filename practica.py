e4527f1d941627361de75c99a49c42078ce2ce3a
Soy Cristian <diacocristian@gmail.com>
2024-08-10 12:45:17 -0300

    update practica.py

--- practica.py
+++ practica.py
@@ -1,9 +1,11 @@
 # ejercicio para practicar python
 
 #FORMA RECURSIVA DE REALIZAR EL PROYECTO
-def sumNum(num):
+def sumNum(num:int) -> int:
+    #caso base
     if num == 1:
         return 1
+    #caso recursivo
     else:
         return num + sumNum(num - 1)
 
